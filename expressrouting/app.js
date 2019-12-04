33b831c7c8785dd5e72f5d5f5949871acf54ac3d
“malhotra-parul” <parulm.business@gmail.com>
2019-12-04 07:53:09 +0530

    Implemented Express routing

--- expressrouting/app.js
+++ expressrouting/app.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
+
+const userRoute = require('./routes/user');
+const managerRoute = require('./routes/manager');
+const adminRoute = require('./routes/admin');
+
 const port = process.env.PORT || 5500;
 
+
+app.use(express.json({extended : false}));
+app.use('/user', userRoute);
+app.use('/admin', adminRoute);
+app.use('/manager', managerRoute);
 
 app.get('/', (req, res) => {
     res.send("Test");
 });
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.send("Done");
+
+})
 
 app.listen(port, () => console.log(`Server started on ${port}`));
---
+++ expressrouting/routes/user.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+router.get('/', (req, res)=>{
+    res.send("I am from User hoomepage.");
+});
+
+module.exports = router;
