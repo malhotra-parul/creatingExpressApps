4e97a65f267bb1e5d1dc12cb50392ade3a7e29ca
“malhotra-parul” <parulm.business@gmail.com>
2019-12-05 20:21:43 +0530

    Used whatsapp click to chat api

--- WhatsappApp/app.js
+++ WhatsappApp/app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 3300;
+const port = process.env.PORT || 3500;
 
 app.use(express.static("views"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
-    var text = `Welcome! ${req.body.fname} ${req.body.lname}. 
-    Your email id is ${req.body.email}. Thank you for your messsage - ${req.body.comment}`;
-    res.send(text);
+app.post("/", (req,res)=>{
+    var text = `Thank you for your query  "${req.body.comment}", ${req.body.fname}. We will get back shortly.`;
+    var final_text = `*Hello There*%0A${text}`;
+    res.redirect(`https://wa.me/${req.body.phone}?text=${final_text}`);
 });
 
 app.listen(port, () => console.log(`Server started on ${port}`));
