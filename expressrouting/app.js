e714fed5dd609d150e612a34627d6013c23e7b38
“malhotra-parul” <parulm.business@gmail.com>
2019-12-05 06:42:06 +0530

    Implementing dummy auth using next()

--- expressrouting/app.js
+++ expressrouting/app.js
@@ -4,6 +4,7 @@ const app = express();
 const userRoute = require('./routes/user');
 const managerRoute = require('./routes/manager');
 const adminRoute = require('./routes/admin');
+const auth = require('./auth');
 
 const port = process.env.PORT || 5500;
 
@@ -18,6 +19,11 @@ app.all("/all/:username", (req, res, next) => {
     console.log(req.params);
     res.send("hello from all!");
 });
+
+app.get("/hit", auth, (req, res, next)=>{
+    console.log("I am second!");
+    res.send("Dummy auth!");
+})
 
 app.get('/', (req, res) => {
     res.send("Test");
--- expressrouting/auth.js
+++ expressrouting/auth.js
@@ -1,1 +1,8 @@
 const express = require('express');
+
+const auth = (req, res, next) => {
+    console.log("You are authenticated to enter!");
+    next();
+}
+
+module.exports = auth;
