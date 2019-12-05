49586857cd85ad47cb68f6816c90b001fbee170d
“malhotra-parul” <parulm.business@gmail.com>
2019-12-05 08:00:23 +0530

    Using async await

--- rest/dbConnect.js
+++ rest/dbConnect.js
@@ -15,10 +15,22 @@ const options = {
 //     console.log("Successfully connected to mongodb!");
 // });
 
-//MEthod 2 - With promises
-mongoose.connect(mongoURI, options)
-    .then(()=> {
-        console.log("Successfully connected to mongo");
-    }, err => {
-        console.log("Something went wrong with mongo!");
-    });
+ //MEthod 2 - With promises
+// mongoose.connect(mongoURI, options)
+//     .then(()=> {
+//         console.log("Successfully connected to mongo");
+//     }, err => {
+//         console.log("Something went wrong with mongo!");
+//     });
+
+//MEthod 3 - async await
+const dbConnect = async() => {
+    try{
+        await mongoose.connect(mongoURI);
+        console.log("Successfully connected!");
+    }
+    catch(err){
+        console.log(err.message);
+    }
+}
+dbConnect();
