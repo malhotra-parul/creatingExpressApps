2d10027698b21e7b511fb02ce957dd12e4c39ae9
“malhotra-parul” <parulm.business@gmail.com>
2019-12-05 08:29:08 +0530

    Included some description

--- rest/app.js
+++ rest/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port =  process.env.PORT || 7700;
+//exporting dbConnect
 const dbConnect = require('./dbConnect');
 
 app.get("/", (req, res)=>{
